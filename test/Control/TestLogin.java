fe9ab2af20c0679b7e8cccdff2877f5f13a6b621
Jonathan Andres Jimenez Trujillo <32826431+jonajitru781@users.noreply.github.com>
2020-05-17 15:11:26 -0500

    Pruebas JUnit

--- test/Control/TestLogin.java
+++ test/Control/TestLogin.java
@@ -5,6 +5,9 @@
  */
 package Control;
 
+import DAO.UsuarioDAO;
+import java.util.ArrayList;
+import Entidad.Usuario;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,12 +20,43 @@ import static org.junit.Assert.*;
  * @author FAMILIA JIMENEZ
  */
 public class TestLogin {
+    private static ValidarLogin validarLogin = new ValidarLogin();
+    
+    private String LONG_NOMBRE_INCORRECTA="Longitud nombre incorrecta";
+    private String LONG_PASSWORD_INCORRECTA="Longitud contraseña incorrecta";
+    private String DATOS_INCORRECTOS="Datos incorrectos";
+    private String USUARIO_AUTORIZADO="Bienvenido";
     
     public TestLogin() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        UsuarioDAO dao = new UsuarioDAO();
+        
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+        
+        Usuario a = new Usuario();
+        Usuario b = new Usuario();
+        Usuario c = new Usuario();
+        
+        a.setNombre("juan");
+        a.setPassword("1234");
+        b.setNombre("pedro");
+        b.setPassword("123");
+        c.setNombre("maria");
+        c.setPassword("12345");
+        
+        usuarios.add(a);
+        usuarios.add(b);
+        usuarios.add(c);
+        
+        for(Usuario u: usuarios){
+            System.out.println(u.getNombre());
+            System.out.println(u.getPassword());
+            System.out.println("------------");
+            dao.crear(u);
+        }
     }
     
     @AfterClass
@@ -42,36 +76,67 @@ public class TestLogin {
     //
     // @Test
     // public void hello() {}
-    
-    
-   @Test
-    public void testContrasenia() {
-        
+    @Test
+    public void testLongitudNombre(){
+        Usuario u=new Usuario();
+        u.setNombre("R");
+        u.setPassword("132456");
+        assertEquals(validarLogin.verificarLogin(u), LONG_NOMBRE_INCORRECTA);
+        u.setNombre("Roberto");
+        u.setPassword("123456");
+        assertEquals(validarLogin.verificarLogin(u),LONG_NOMBRE_INCORRECTA);
     }
     
     @Test
-    public void testDatos() {
+    public void testLongitudContrasenia(){
+        Usuario u = new Usuario();
+        u.setNombre("Pepe");
+        u.setPassword("12");
+        assertEquals(validarLogin.verificarLogin(u), LONG_PASSWORD_INCORRECTA);
         
+        u.setNombre("Pepe");
+        u.setPassword("123456");
+        assertEquals(validarLogin.verificarLogin(u), LONG_PASSWORD_INCORRECTA);
     }
     
     @Test
-    public void testLongitudContrasenia() {
-        
+    public void testNombre(){
+        Usuario u=new Usuario();
+        u.setNombre("Henry");
+        u.setPassword("12345");
+        assertEquals(validarLogin.verificarLogin(u), DATOS_INCORRECTOS);
     }
     
     @Test
-    public void testLongitudNombre() {
-        
+    public void testContrasenia(){
+        Usuario u=new Usuario();
+        u.setNombre("maria");
+        u.setPassword("1234");
+        assertEquals(validarLogin.verificarLogin(u), DATOS_INCORRECTOS);
     }
     
     @Test
-    public void todoCorrecto() {
-        
+    public void testDatos(){
+        Usuario u=new Usuario();
+        u.setNombre("Henry");
+        u.setPassword("A234");
+        assertEquals(validarLogin.verificarLogin(u), DATOS_INCORRECTOS);
     }
     
     @Test
-    public void testNombre() {
+    public void testTodoCorrecto(){
+        Usuario u=new Usuario();
         
+        u.setNombre("juan");
+        u.setPassword("1234");
+        assertEquals(validarLogin.verificarLogin(u), USUARIO_AUTORIZADO);
+        
+        u.setNombre("pedro");
+        u.setPassword("123");
+        assertEquals(validarLogin.verificarLogin(u), USUARIO_AUTORIZADO);
+        
+        u.setNombre("maria");
+        u.setPassword("12345");
+        assertEquals(validarLogin.verificarLogin(u), USUARIO_AUTORIZADO);
     }
-    
 }
